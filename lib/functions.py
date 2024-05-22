9d1eaf53da3e18d11f6ab497d3244004c26aed30
james nyakundi <omarejames082@gmail.com>
2024-05-22 12:49:49 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,49 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello,programmer!")
+    greet_programmer()
     pass
 
 def greet(name):
-    pass
+  print("Hello, {}!".format(name))
 
+# You can call the function like this:
+greet("Naureen")
+pass
 def greet_with_default(name="programmer"):
-    pass
+    print("Hello, {}!".format(name))
+
+# Call the function with no arguments
+greet_with_default()
+
+# Call the function with one argument
+greet_with_default("Sunny")
+
+pass
 
 def add(num1, num2):
-    pass
+    
+    return num1 + num2
+
+# Call the function with two arguments and print the return value
+sum_result = add(1, 2)
+print(sum_result)
+
+pass
 
 def halve(number):
-    pass
+    
+    if not isinstance(number, (int, float)):
+        return None
+    
+    return number / 2
+
+# Test cases
+result1 = halve(4)
+print(result1)  # Output: 2
+
+result2 = halve("two")
+print(result2)  # Output: None
+
+pass
